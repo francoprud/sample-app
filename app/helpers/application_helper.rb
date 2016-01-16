5708074db9d66074fb1d2e6a9f6afbf27c9e7b3d
Franco Prudhomme <francoprud@gmail.com>
2016-01-16 15:26:08 -0300

    Adds helper for title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    return "#{page_title} | #{base_title}" if page_title.present?
+    base_title
+  end
 end
